dcdf55b34e223756be9312103586e81886872380
JaySingh <jaysingh1452@gmail.com>
2024-01-19 15:17:09 +0530

    fourth-lab

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const addTaskForm = document.getElementById('add-task-form');
 
+    addTaskForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const taskInput = document.getElementById('task-input');
+        const taskText = taskInput.value.trim();
+
+        if (taskText !== '') {
+            addTaskToList(taskText);
+            taskInput.value = '';
+        }
+	});
+
+});
